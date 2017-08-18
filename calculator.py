595da06c994068efe52e9796f50c887f96c395d4
jorgebonilla <jorge.luis.bonilla@gmail.com>
2017-08-18 16:20:08 -0700

    upload

--- calculator.py
+++ calculator.py
@@ -1,6 +1,15 @@
-from flask import render_template
+from flask import Flask, url_for, render_template, send_from_directory
 
-@app.route('/calculator/')
-@app.route('/calculator/<name>')
+app = Flask(__name__)
+
+@app.route('/')
 def calculator():
     return render_template('calculator.html')
+
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    return send_from_directory(os.path.join(root_dir, 'static'), filename)
+
+if __name__ == '__main__':
+    app.run()
